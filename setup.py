db0bee36e2a330af6869a34abbfdefc5b2cafee0
vincent d warmerdam <vincentwarmerdam@gmail.com>
2022-06-15 15:17:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="bulk",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=["typer>=0.4.1", "bokeh>=2.4.3"],
+    install_requires=["typer>=0.4.1", "bokeh>=2.4.3", "pandas>=1.0.0"],
     entry_points={
         'console_scripts': [
             'bulk = bulk.__main__:app',
