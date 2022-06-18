ecbf7677c7b313f0aaba8f90d9f3074ffd3d2abc
Vincent Warmerdam <vincentwarmerdam@MacBook-Air-van-Vincent.local>
2022-06-18 19:46:33 +0200

    add-dev-deps

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version="0.0.2",
     packages=find_packages(),
     install_requires=["typer>=0.4.1", "bokeh>=2.4.3", "pandas>=1.0.0"],
+    extras_require={
+        "dev": ["pytest-playwright==0.3.0"],
+    },
     entry_points={
         'console_scripts': [
             'bulk = bulk.__main__:app',
