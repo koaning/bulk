a74bb106974058b9512842ad3ffd221481b54291
koernerfelicia <45405119+koernerfelicia@users.noreply.github.com>
2022-09-14 11:28:53 +0200

    Fix incorrect import, deprecation warning, and formatting

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import pytest
-from pandas import DataFrame
 
-from bulk.utils import get_color_mapping
+from bulk._bokeh_utils import get_color_mapping
 
 MAX_DISCRETE_CLASSES = 10
+
 
 def _int_to_alpha(x: int) -> str:
     # returns int mapped to char: 0: a, 1: b, 2: c
     return chr(x + 97)
 
 
-def _create_dummy_df() -> DataFrame:
+def _create_dummy_df() -> pd.DataFrame:
     df = pd.DataFrame()
     df["color"] = [_int_to_alpha(i) for i in range(MAX_DISCRETE_CLASSES)]
     return df
@@ -19,8 +19,8 @@ def _create_dummy_df() -> DataFrame:
 
 def test_get_color_mapping_raises_error_on_too_many_classes():
     df = _create_dummy_df()
-    additional_class = {"color": _int_to_alpha(MAX_DISCRETE_CLASSES + 1)}
-    df = df.append(additional_class, ignore_index=True)
+    additional_class = pd.DataFrame.from_records([{"color": _int_to_alpha(MAX_DISCRETE_CLASSES + 1)}])
+    df = pd.concat([df, additional_class], axis=0, ignore_index=True)
     with pytest.raises(ValueError):
         get_color_mapping(df)
 
