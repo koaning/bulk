9e73ee2043bc9ebcbdce0de082fc17630f3b25bc
Vincent <vincentwarmerdam@gmail.com>
2023-01-29 11:33:17 +0100

    my-first-test

--- js/tests.js
+++ js/tests.js
@@ -22,10 +22,9 @@ function table_to_csv(source) {
 
 
 var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+
+describe('Super basic test', function () {
+    it('1 == 1', function () {
+        assert.equal(1, 1);
     });
-  });
 });
