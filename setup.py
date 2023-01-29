c5bb10b53ef8586c99ae6f90182078f57b03c279
Vincent <vincentwarmerdam@gmail.com>
2023-01-29 13:33:48 +0100

    upgrade version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bulk",
-    version="0.1.4",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=["typer>=0.4.1,<1.0.0", "bokeh>=2.4.3,<3.0.0", "pandas>=1.0.0,<2.0.0", "wasabi>=0.9.1"],
     extras_require={
