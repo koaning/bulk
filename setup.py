c423e50328cd268e81b1d65b0ca030cec67cbbfd
vincent d warmerdam <vincentwarmerdam@gmail.com>
2024-07-18 14:49:00 +0000

    progress

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="bulk",
     version="0.3.1",
     packages=find_packages(),
-    install_requires=["radicli>=0.0.8,<0.1.0", "bokeh>=2.4.3,<3.0.0", "pandas>=1.0.0", "wasabi>=0.9.1"],
+    install_requires=["radicli>=0.0.8,<0.1.0", "bokeh>=2.4.3,<3.0.0", "pandas>=1.0.0", "wasabi>=0.9.1", "numpy<2", "jupyter-scatter"],
     extras_require={
         "dev": ["pytest-playwright==0.3.0"],
     },
