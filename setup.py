01308e9c68ba3ee05b2050989dbd893cf6c16ca7
vincent d warmerdam <vincentwarmerdam@gmail.com>
2024-07-22 14:03:05 +0000

    release

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="bulk",
-    version="0.3.1",
+    version="0.3.2",
     packages=find_packages(),
-    install_requires=["radicli>=0.0.8,<0.1.0", "bokeh>=2.4.3,<3.0.0", "pandas>=1.0.0", "wasabi>=0.9.1", "numpy<2", "jupyter-scatter"],
+    install_requires=["radicli>=0.0.8,<0.1.0", "bokeh>=2.4.3,<3.0.0", "pandas>=1.0.0", "wasabi>=0.9.1", "numpy<2", "jupyter-scatter", "scikit-learn"],
     extras_require={
         "dev": ["pytest-playwright==0.3.0"],
     },
